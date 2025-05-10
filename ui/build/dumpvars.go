8a75746f5688b730c5f18b7dcfac01ab2d6de5ec
Zabuka_zuzu <zuzu950444@gmail.com>
2025-05-10 17:17:11 +0000

    soong: Add Mist build info to dumpvars

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -147,7 +147,8 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"CR_VERSION",
+	"MIST_BUILD_INFO",
+	"MISTOS_MAINTAINER",
 	"PRODUCT_SOURCE_ROOT_DIRS",
 	"TARGET_DEVICE",
 	"TARGET_BUILD_VARIANT",
