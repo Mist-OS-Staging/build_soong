ee49a4086a1360bd2914042e1063ccd4c05174e5
Zabuka_zuzu <zuzu950444@gmail.com>
2025-11-16 04:50:31 +0000

    Revert "build: soong: use jemalloc by default and allow opt-in to scudo (2/4)"
    This reverts commit 76ebdc4186f4c4760f0ae8b510bc30796e8d6136.

--- android/variable.go
+++ android/variable.go
@@ -75,16 +75,6 @@ type variableProperties struct {
 			Header_libs         []string `android:"arch_variant"`
 		} `android:"arch_variant"`
 
-		Malloc_use_jemalloc struct {
-			Cflags              []string `android:"arch_variant"`
-			Shared_libs         []string `android:"arch_variant"`
-			Whole_static_libs   []string `android:"arch_variant"`
-			Static_libs         []string `android:"arch_variant"`
-			Exclude_static_libs []string `android:"arch_variant"`
-			Srcs                []string `android:"arch_variant"`
-			Header_libs         []string `android:"arch_variant"`
-		} `android:"arch_variant"`
-
 		Malloc_zero_contents struct {
 			Cflags []string `android:"arch_variant"`
 		} `android:"arch_variant"`
@@ -300,7 +290,6 @@ type ProductVariables struct {
 	Always_use_prebuilt_sdks     *bool    `json:",omitempty"`
 	Skip_boot_jars_check         *bool    `json:",omitempty"`
 	Malloc_low_memory            *bool    `json:",omitempty"`
-	Malloc_use_jemalloc          *bool    `json:",omitempty"`
 	Malloc_zero_contents         *bool    `json:",omitempty"`
 	Malloc_pattern_fill_contents *bool    `json:",omitempty"`
 	Safestack                    *bool    `json:",omitempty"`
@@ -336,6 +325,8 @@ type ProductVariables struct {
 	EnableCFI       *bool    `json:",omitempty"`
 	CFIExcludePaths []string `json:",omitempty"`
 	CFIIncludePaths []string `json:",omitempty"`
+
+	DisableScudo *bool `json:",omitempty"`
 
 	MemtagHeapExcludePaths      []string `json:",omitempty"`
 	MemtagHeapAsyncIncludePaths []string `json:",omitempty"`
@@ -777,7 +768,6 @@ func (v *ProductVariables) SetDefaultConfig() {
 		AAPTPrebuiltDPI:     []string{"xhdpi", "xxhdpi"},
 
 		Malloc_low_memory:            boolPtr(false),
-		Malloc_use_jemalloc:          boolPtr(true),
 		Malloc_zero_contents:         boolPtr(true),
 		Malloc_pattern_fill_contents: boolPtr(false),
 		Safestack:                    boolPtr(false),
